1afacbc8b9c3d18c9e9ec013bda7457c0af3aad2
Gentil Quentin <q.rurangirw@alustudent.com>
2024-06-19 19:40:59 +0200

    gentil

---
+++ script.js
@@ -0,0 +1,7 @@
+let submit = document.querySelector(".submit-button")
+let input = document.querySelector(".input")
+
+submit.addEventListener("click", function(){
+    alert("Thank You For your submission!")
+    
+})
